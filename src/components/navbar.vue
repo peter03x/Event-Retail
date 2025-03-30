396e1dc845322115dfb3450f9c9a7a4e6458a1c8
Hoang Bao Phuc Chau <103523966@student.swin.edu.au>
2025-03-31 02:20:13 +1100

    Adding Responsiveness to NavBar and Home View

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar --bs-warning-bg-subtle">
+  <nav class="navbar --bs-warning-bg-subtle navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
       <a class="navbar-brand" href="/">
         <img
@@ -13,32 +13,42 @@
         <span class="text-dark fw-bold" id="brandName">Teket</span>
       </a>
 
-      <ul class="nav justify-content-end">
-        <li class="nav-item">
-          <a href="/events" style="font-family: 'Font'" class="nav-link">
-            <img
-              src="../assets/calendar.png"
-              alt="ICON"
-              class="d-inline-block align-text-middle"
-              width="20"
-              height="20"
-            />
-            <span class="text-warning ms-2 fw-semibold">Events</span>
-          </a>
-        </li>
-        <li class="nav-item ms-4">
-          <router-link to="/signIn">
-            <button class="btn btn-warning fw-semibold">Sign In</button>
-          </router-link>
-        </li>
-        <li class="nav-item ms-3">
-          <router-link to="/signUp"
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+              data-bs-target="#navbarContent" aria-controls="navbarContent"
+              aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0 ">
+          <li class="nav-item d-flex justify-content-center">
+            <a href="/events" style="font-family: 'Font'" class="nav-link">
+              <img
+                  src="../assets/calendar.png"
+                  alt="ICON"
+                  class="d-inline-block align-text-middle"
+                  width="20"
+                  height="20"
+              />
+              <span class="text-warning ms-lg-2 fw-semibold just">Events</span>
+            </a>
+          </li>
+          <li class="nav-item ms-lg-4">
+            <router-link to="/signIn">
+              <button class="btn btn-warning fw-semibold">Sign In</button>
+            </router-link>
+          </li>
+          <li class="nav-item ms-lg-3">
+            <router-link to="/signUp"
             ><button class="btn btn-light fw-semibold">
               Sign Up
             </button></router-link
-          >
-        </li>
-      </ul>
+            >
+          </li>
+        </ul>
+      </div>
+
+
     </div>
   </nav>
 </template>
@@ -50,5 +60,12 @@ import { useRouter } from "vue-router";
 <style scoped>
 #brandName {
   font-family: "LogoFont";
+}
+
+@media (max-width: 992px) {
+  .navbar-nav .btn {
+    width: 100%;
+    margin: 5px 0;
+  }
 }
 </style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,71 +1,90 @@
 <template>
   <navbar />
-  <div class="card text-bg-light">
-    <img
-      src="../assets/img1Home.jpg"
-      alt="img"
-      class="card-img"
-      style="object-fit: fill; height: 40vh"
-    />
-    <div class="card-img-overlay ms-5">
-      <h2
-        class="card-title text-light fs-1 fw-bold ms-5"
-        style="font-family: 'Font'"
-      >
-        Hello folks!<br />
-        Welcome to Teket!
-      </h2>
-      
-      <p class="card-text text-light fs-4 ms-5" style="font-family: 'Font'">
-        Tickets are available here<br />
-        for anyone with supper low price!
-      </p>
-      <br />
-      <router-link to="/events">
-        <button
-          type="button"
-          class="btn btn-warning ms-5 fs-4 mb-2"
-          style="font-family: 'Font'"
-        >
-          Let's start
-        </button>
-      </router-link>
-    </div>
-  </div>
-  <div>
-    <h4 style="font-family: 'Font'" class="fw-bold pt-5 ms-5">
-      Explore Our Latest Event
-    </h4>
+  <div class="container-fluid p-0">
+    <div class="text-bg-light banner-img pt-4 ps-lg-5">
+      <div class="container-fluid">
+        <div class = "d-flex justify-content-center justify-content-xl-start
+        justify-content-md-center justify-content-sm-center">
+          <h2
+              class="text-light fs-1 fw-bold "
+              style="font-family: 'Font'"
+          >
+            Hello folks!
+          </h2>
+        </div>
+        <div class = " d-flex justify-content-center justify-content-xl-start justify-content-md-center justify-content-sm-center">
+          <h2
+              class="text-light fs-1 fw-bold "
+              style="font-family: 'Font'"
+          >
+            Welcome to Teket!
+          </h2>
+        </div>
 
-    <div class="input-group row ps-5 mt-4">
-      <div class="col-4" style="padding-left: 2%">
-        <input
-          type="text"
-          class="form-control"
-          placeholder="Search"
-          aria-label="Search"
-          aria-describedby="addon-wrapping"
-        />
+        <div class = " d-flex justify-content-center justify-content-xl-start justify-content-md-center justify-content-sm-center">
+          <span class="text-light  " style="font-family: 'Font'">
+            Tickets are available here
+          </span>
+        </div>
+
+        <div class = " d-flex justify-content-center justify-content-xl-start justify-content-md-center justify-content-sm-center ">
+          <span class="text-light  " style="font-family: 'Font'">
+            for anyone with supper low price!
+          </span>
+        </div>
+        <br />
+        <div class = "d-flex justify-content-center justify-content-xl-start justify-content-md-center justify-content-sm-center">
+          <router-link to="/events">
+            <button
+                type="button"
+                class="btn btn-warning mb-4"
+                style="font-family: 'Font'"
+            >
+              Let's start
+            </button>
+          </router-link>
+        </div>
       </div>
-      <div class="col-4"></div>
     </div>
 
-    <h5
-      style="font-family: 'Font'"
-      class="fw-bold mt-5 ps-5 text-body-tertiary"
-    >
-      No upcoming events
-    </h5>
-    <div class="container text-center">
-      <router-link to="/events">
-        <button
-          type="button"
-          class="btn fw-semibold btn-lg align-self-center"
-          style="font-family: 'Font'; background-color: #edece8"
-        >
-          See all events
-        </button>
-      </router-link>
+    <div class = "container-fluid">
+      <div class = "d-flex justify-content-center justify-content-lg-start justify-content-sm-center">
+        <h4 style="font-family: 'Font'" class="fw-bold pt-5 ps-lg-5">
+          Explore Our Latest Event
+        </h4>
+      </div>
+
+      <div class = "row">
+        <div class="col-lg-4 col-md-12 ">
+          <div class=" input-group ps-lg-5">
+            <input
+                type="text"
+                class="form-control"
+                placeholder="Search"
+                aria-label="Search"
+                aria-describedby="addon-wrapping"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div class = "d-flex justify-content-center justify-content-lg-start justify-content-sm-center">
+        <h5 style="font-family: 'Font'" class="fw-bold mt-5 text-body-tertiary ps-lg-5">
+          No upcoming events
+        </h5>
+      </div>
+
+      <div class="container text-center mt-4">
+        <router-link to="/events">
+          <button
+              type="button"
+              class="btn fw-semibold btn-lg align-self-center"
+              style="font-family: 'Font'; background-color: #edece8"
+          >
+            See all events
+          </button>
+        </router-link>
+      </div>
     </div>
   </div>
   <section
@@ -74,7 +93,7 @@
   >
     <div class="container">
       <div class="row align-items-center">
-        <div class="col-lg-6 text-center text-lg-start">
+        <div class="col-lg-6 text-center text-lg-start mt-4">
           <h1 class="fw-semibold">
             Teket is for <strong>anyone</strong> who wants to make a difference!
           </h1>
@@ -104,7 +123,7 @@
             class="img-fluid hero-image pt-4 pb-4 rounded"
           />
         </div>
-        <div class="col-lg-6">
+        <div class="col-lg-6 text-center text-lg-start mb-4">
           <h1 class="fw-semibold">Join and start enjoying our offers</h1>
           <p>Let Teket take care of your boring days.</p>
           <a href="#" class="btn btn-light btn-lg" style="font-family: 'Font';">Create an account</a>
@@ -118,50 +137,53 @@
     <h5 class="text-muted">
       Teket is an event retail platform. Get in touch to find out more.
     </h5>
-    <div
-      style="display: flex"
-      class="mt-4 justify-content-center align-items-center"
-    >
-      <img alt="ava" src="../assets/avatar.jpg" />
-      <div class="text-start">
-        <h3 class="fw-bold text-warning">Mason Mount</h3>
-        <h5 class="text-muted mb-4">Co-founder</h5>
-        <div style="display: flex" class="mt-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-mail h-5 w-5"
-          >
-            <rect width="20" height="16" x="2" y="4" rx="2"></rect>
-            <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
-          </svg>
-          <p class="ms-2">abcd@gmail.com</p>
+    <div style="display: flex" class="mt-4 justify-content-center  container">
+      <div class = "row">
+        <div class = "col-lg-6">
+          <img alt="ava" class = "profile-img " src="../assets/avatar.jpg" />
         </div>
-        <div style="display: flex">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-phone h-5 w-5"
-          >
-            <path
-              d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"
-            ></path>
-          </svg>
-          <p class="ms-2">0433990388</p>
+        <div class = "col-lg-6 align-self-center">
+          <div class="ps-lg-4">
+            <h3 class="fw-bold text-warning text-lg-start">Mason Mount</h3>
+            <h5 class="text-muted text-lg-start mb-4">Co-founder</h5>
+            <div class="mt-2 d-flex flex-row justify-content-center justify-content-lg-start">
+              <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="lucide lucide-mail h-5 w-5"
+              >
+                <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+              </svg>
+              <p class="ms-2">abcd@gmail.com</p>
+            </div>
+            <div class="mt-2 d-flex flex-row justify-content-center justify-content-lg-start">
+              <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="lucide lucide-phone h-5 w-5"
+              >
+                <path
+                    d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"
+                ></path>
+              </svg>
+              <p class="ms-2">0433990388</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -182,7 +204,28 @@ export default {
 </script>
 
 <style scoped>
+
+
 h.p {
   font-family: "Font";
+}
+
+.banner-img{
+  background-image: url("../assets/img1Home.jpg");
+  background-position: center;
+  background-size: cover;
+}
+
+.profile-img {
+  object-fit: cover;
+  height: 300px;
+  width: 300px;/* Ensures image covers the area without distortion */
+}
+
+@media (max-width: 768px) {
+  .profile-img{
+    width: 150px;
+    height: 150px;
+  }
 }
 </style>
